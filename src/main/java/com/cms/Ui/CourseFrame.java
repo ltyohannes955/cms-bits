28b198194f90c9206bf2e64a9d16a9170c637cf2
Dagmawit.T <Dagitesh2188@gmail.com>
2022-11-15 09:18:40 +0300

    new changes on Delete Course

--- src/main/java/com/cms/Ui/CourseFrame.java
+++ src/main/java/com/cms/Ui/CourseFrame.java
@@ -55,7 +55,7 @@ public class CourseFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
         jLabel1.setText("COURSE MANAGMENT SYSTEM ");
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
